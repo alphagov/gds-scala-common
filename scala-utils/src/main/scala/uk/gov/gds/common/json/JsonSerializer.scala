752b6f4346874b6bad502fd5b5b2902c6c82ca87
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2012-05-04 10:55:52 +0100

    Added logging when json serialization fails

--- scala-utils/src/main/scala/uk/gov/gds/common/json/JsonSerializer.scala
+++ scala-utils/src/main/scala/uk/gov/gds/common/json/JsonSerializer.scala
@@ -1,10 +1,25 @@
 package uk.gov.gds.common.json
 
 import com.codahale.jerkson.Json._
+import uk.gov.gds.common.logging.Logging
 
-object JsonSerializer {
+object JsonSerializer extends Logging {
 
-  def toJson[A](obj: A)(implicit m: Manifest[A]): String = generate[A](obj)
+  def toJson[A](obj: A)(implicit m: Manifest[A]): String = try {
+    generate[A](obj)
+  }
+  catch {
+    case e: Exception =>
+      logger.error("Could not serialize TO json from: " + obj, e)
+      throw e
+  }
 
-  def fromJson[A](json: String)(implicit m: Manifest[A]) = parse[A](json)
+  def fromJson[A](json: String)(implicit m: Manifest[A]) = try {
+    parse[A](json)
+  }
+  catch {
+    case e: Exception =>
+      logger.error("Could not serialize FROM json: " + json, e)
+      throw e
+  }
 }
