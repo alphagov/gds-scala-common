a5783fa46f6717417ed2028d03255618bc6921ee
David Illsley <david@illsley.org>
2012-07-22 12:57:23 +0100

    Move across remaining calendar tests

--- scala-utils/src/test/scala/uk/gov/gds/common/calendar/CalendarTests.scala
+++ scala-utils/src/test/scala/uk/gov/gds/common/calendar/CalendarTests.scala
@@ -3,7 +3,7 @@ package uk.gov.gds.common.calendar
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import uk.gov.gds.common.logging.Logging
-import org.joda.time.LocalDate
+import org.joda.time.{DateTime, LocalDate}
 
 class CalendarTests extends FunSuite
 with ShouldMatchers
@@ -20,4 +20,15 @@ with Logging {
     CalendarApiClient.getBankHolidays("ni", 2012) should contain(LocalDate.parse("2012-07-12"))
   }
 
+  test("Next working day after Good Thursday is the Tuesday after Easter") {
+    Calendar.plusDaysWithHolidays("England", DateTime.parse("2012-04-05"), 1) should be(DateTime.parse("2012-04-10"))
+  }
+
+  test("7 working days after 2012-12-28 is 2013-01-09 (New Year Holiday)") {
+    Calendar.plusDaysWithHolidays("england", DateTime.parse("2012-12-28"), 7) should be(DateTime.parse("2013-01-09"))
+  }
+
+  test("5 working days after Sunday should be the Monday after next") {
+    Calendar.plusDaysWithHolidays("england", DateTime.parse("2012-07-01"), 5) should be(DateTime.parse("2012-07-09"))
+  }
 }
