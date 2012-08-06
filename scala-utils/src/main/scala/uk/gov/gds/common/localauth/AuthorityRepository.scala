2f3018ad57d4955a0cde4153243f9bfe86106168
Mat Wall <mwall@alphagov.co.uk>
2012-08-06 12:06:37 +0100

    Aded authority repository for places & licensify

---
+++ scala-utils/src/main/scala/uk/gov/gds/common/localauth/AuthorityRepository.scala
@@ -0,0 +1,32 @@
+package uk.gov.gds.common.localauth
+
+import javassist.NotFoundException
+import uk.gov.gds.common.mongo.repository.{Ascending, IdentityBasedMongoRepository}
+
+abstract class AuthorityRepository extends IdentityBasedMongoRepository[Authority] {
+
+  protected val databaseIdProperty = "urlSlug"
+
+  override def createIndexes() {
+    super.createIndexes()
+    addIndex(index("agencyId" -> Ascending))
+    addIndex(index("snacCode" -> Ascending))
+  }
+
+  def getAgencyIdFromSnacCode(snacCode: String) = loadBySnacCode(snacCode) match {
+    case Some(authority) => authority.agencyId
+    case _ => throw new NotFoundException("snacCode " + snacCode + " not found")
+  }
+
+  def loadByAgencyId(agencyId: Int): Option[Authority] = collection.findOne(where("agencyId" -> agencyId))
+
+  def loadByUrlSlug(urlSlug: String) = load(urlSlug)
+
+  def loadBySnacCode(snacCode: String): Option[Authority] = collection.findOne(where("snacCode" -> snacCode))
+
+  def loadListBySnacCodes(strings: List[String]): List[Authority] = collection.find(where("snacCode" -> in(strings)))
+
+  def updateAuthority(urlSlug: String, newAgencyId: Int) {
+    collection.update(where("urlSlug" -> urlSlug), update("$set" -> query("agencyId" -> newAgencyId)))
+  }
+}
