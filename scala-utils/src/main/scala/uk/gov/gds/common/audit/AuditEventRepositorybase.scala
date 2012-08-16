754e48010662da3b8c9c404a2e94b31ef062c8da
Leszek Gonczar <gonczar@gmail.com>
2012-08-16 13:48:52 +0100

    Overrode AuditEventRepositoryBase.audit method

--- scala-utils/src/main/scala/uk/gov/gds/common/audit/AuditEventRepositorybase.scala
+++ scala-utils/src/main/scala/uk/gov/gds/common/audit/AuditEventRepositorybase.scala
@@ -9,8 +9,17 @@ abstract class AuditEventRepositoryBase extends SimpleMongoRepository[AuditEvent
 
   def audit(event: AuditEvent) {
     logger.info(event.toString)
+
+    // TODO: (LG) perhaps we should try-catch the unsafeInsert and log the exception (probably we don't even want to rethrow it)
     unsafeInsert(event)
   }
+  
+  def audit(auditType: String, tags: Map[String, String], detail: Map[String, String]) : Unit =
+    audit(AuditEvent(
+      auditType = auditType,
+      tags = tags,
+      detail = detail
+    ))
 
   override protected def createIndexes() {
     super.createIndexes()
