8278527ff2b86b4473cda9be9eaa3c7a131eee03
Mat Wall <mwall@alphagov.co.uk>
2012-09-20 12:03:50 +0100

    Added file mime type detection

--- scala-utils/src/main/scala/uk/gov/gds/common/clamav/ClamAntiVirus.scala
+++ scala-utils/src/main/scala/uk/gov/gds/common/clamav/ClamAntiVirus.scala
@@ -7,7 +7,7 @@ import java.net.{InetSocketAddress, Socket}
 
 class ClamAntiVirus(streamCopyFunction: (InputStream) => Unit = DevNull.nullStream(_),
                     virusDetectedFunction: => Unit = (),
-                    allowedMimeTypes: List[String])
+                    allowedMimeTypes: List[String] = Nil)
   extends ClamAvConfig {
 
   private val copyInputStream = new PipedInputStream()
