589e7f1183eb0f6a07ab5ea942c02ab6ef0d88cf
Leszek Gonczar <gonczar@gmail.com>
2012-09-24 10:43:25 +0100

    RequestLogging - logging query string instead of request body

--- scala-utils/src/main/scala/uk/gov/gds/common/play/logging/RequestLogger.scala
+++ scala-utils/src/main/scala/uk/gov/gds/common/play/logging/RequestLogger.scala
@@ -19,7 +19,7 @@ trait RequestLogging {
         val end = new DateTime()
         Logger.info(
           "Time taken to " + ctx.method +
-            " params " + ctx.body +
+            " query string params " + ctx.queryString +
             " path " + ctx.path +
             " was " + end.minus(start.getMillis).getMillis)
         response
