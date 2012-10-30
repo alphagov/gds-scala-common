0fd5985ba999527ab4430ffc9eb2152bb6992b5b
Mat Wall <mwall@alphagov.co.uk>
2012-10-30 14:32:02 +0000

    Changed noisy log message to debug

--- scala-utils/src/main/scala/uk/gov/gds/common/repository/Cursor.scala
+++ scala-utils/src/main/scala/uk/gov/gds/common/repository/Cursor.scala
@@ -49,7 +49,7 @@ abstract class CursorBase[A](var pageSize: Int,
     logger.trace(logMessage)
     val startTimeInMillis = System.currentTimeMillis()
     val queryResult = query
-    logger.debug(logMessage + " completed in " + (System.currentTimeMillis() - startTimeInMillis) + "ms")
+    logger.trace(logMessage + " completed in " + (System.currentTimeMillis() - startTimeInMillis) + "ms")
     queryResult
   }
 
