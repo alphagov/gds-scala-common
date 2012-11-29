86546906b157aacdec1970feaa2714508ae95f20
Mat Wall <mwall@alphagov.co.uk>
2012-11-29 16:19:52 +0000

    Cleaned up provider. Ensured that exception is logged if a dependency is resolved without Guice having been initialised.

--- govuk-guice-utils/src/main/scala/uk/gov/gds/guice/DependencyInjectionProvider.scala
+++ govuk-guice-utils/src/main/scala/uk/gov/gds/guice/DependencyInjectionProvider.scala
@@ -11,8 +11,6 @@ import com.google.inject.Injector
  */
 trait DependencyInjectionProvider {
 
-  import GuiceContainer.injector
-
   /**
    * This method will return a dependency from the dependency injection framework. It is designed to be used from
    * objects that are not managed by the dependency injection framework, such as play controllers. It only allows for
@@ -22,34 +20,32 @@ trait DependencyInjectionProvider {
    * @return instance of class resolved from dependency injection framework
    */
 
-  @inline protected final def dependency[A <: AnyRef](implicit m: Manifest[A]) =
-    injector.getInstance(m.erasure.asInstanceOf[Class[A]])
-
-  @inline protected final def initalizeDependencyInjector() {
-    injector.getAllBindings
-  }
+  @inline protected final def dependency[A <: AnyRef](implicit m: Manifest[A]) = GuiceContainer.dependency[A]
 }
 
 object GuiceContainer {
 
   private var di: Injector = null;
 
-  @inline private[guice] final def injector =
-    if (di == null)
-      throw new IllegalStateException("Guice is not initialise. You must call initialize first!")
-    else
-      di
+  /**
+   * Call this method in a static context when your app starts up, within Global in play for example,
+   * to initialize guice. You can pass in a module to configure the DI container if required, but this is optional.
+   * If you don't do this then a default empty module will be used, which may be sufficient for most simple
+   * applications.
+   */
 
   def initialize(module: Module = DefaultModuleProvider.defaultModule) = performInit(List(module))
 
+  /**
+   * Call this method in a static context when your app starts up, within Global in play for example,
+   * to initialize guice. You can pass in an array of modules to configure the DI container. This method is provided
+   * for the case when you want to initialise the DI container with an array of configuration modules.
+   */
+
   def initialize(modules: List[Module]) = performInit(modules)
 
-  @inline protected final def dependency[A <: AnyRef](implicit m: Manifest[A]) =
-    di.getInstance(m.erasure.asInstanceOf[Class[A]])
-
-  protected def initalizeDependencyInjector() {
-    di.getAllBindings
-  }
+  @inline private[guice] final def dependency[A <: AnyRef](implicit m: Manifest[A]) =
+    injector.getInstance(m.erasure.asInstanceOf[Class[A]])
 
   @inline private final def performInit(modules: List[Module]) {
     synchronized {
@@ -59,6 +55,12 @@ object GuiceContainer {
       di = Guice.createInjector(modules.toSeq: _*)
     }
   }
+
+  @inline private final def injector =
+    if (di == null)
+      throw new IllegalStateException("Guice is not initialised. You must call initialize first!")
+    else
+      di
 }
 
 private[guice] object DefaultModuleProvider {
