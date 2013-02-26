cc39a0307abd9485995230b61c37e9334928219a
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2013-02-26 12:14:16 +0000

    Upgraded to the 2.10 version of the idea plugin.
    SBT command is now ./sbt gen-idea to generate project files.

--- project/GdsScalaCommonBuild.scala
+++ project/GdsScalaCommonBuild.scala
@@ -1,29 +1,17 @@
 import sbt._
-import org.sbtidea.SbtIdeaPlugin
 
 object GdsScalaCommonBuild extends Build {
 
-  def ideaSettings = {
-    SbtIdeaPlugin.ideaSettings ++
-      Seq(
-        SbtIdeaPlugin.commandName := "idea"
-      )
-  }
-
-  val root = Project("gds-common", file("."))
+    val root = Project("gds-common", file("."))
 	 .aggregate(scalaUtils, mongoScalaUtils, govUkClients, govUkGuiceUtils)
   
   lazy val scalaUtils = Project("scala-utils", file("scala-utils"))
-	  .settings(ideaSettings: _*)
 
   lazy val mongoScalaUtils = Project("mongo-utils", file("mongo-utils"))
-    .settings(ideaSettings: _*)
     .dependsOn(scalaUtils % "test->test;test->compile;compile->compile")
 
   lazy val govUkClients = Project("govuk-clients", file("govuk-clients"))
-    .settings(ideaSettings: _*)
     .dependsOn(scalaUtils % "test->test;test->compile;compile->compile")
 
   lazy val govUkGuiceUtils = Project("govuk-guice-utils", file("govuk-guice-utils"))
-    .settings(ideaSettings: _*)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,7 +5,7 @@ resolvers ++= Seq(
     "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 )
 
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0-M2-TYPESAFE")
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0")
 
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.1")
 
